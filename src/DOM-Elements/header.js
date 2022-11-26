234980a66762a7bb2e6e200964d6c84502531b29
Gina Henderson <gina@ginahenderson.me>
2022-11-25 20:00:07 -0600

    Add about btn

--- src/DOM-Elements/header.js
+++ src/DOM-Elements/header.js
@@ -1,6 +1,17 @@
+import modal from "./modal";
+
 export default (() => {
   const container = document.createElement('header');
-  container.textContent = 'HEADER';
+  const title = document.createElement('h1');
+  title.textContent = 'Where\'s SpongeBob?';
+  const about = document.createElement('button');
+  about.textContent = 'About';
+  about.addEventListener('click', () => modal.open());
+
+  container.append(
+    title,
+    about,
+  );
 
   return container;
 })();
