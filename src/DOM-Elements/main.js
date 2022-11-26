ee3763befed42b997703528c2faadd60cca6c32c
Gina Henderson <gina@ginahenderson.me>
2022-11-25 19:40:26 -0600

    Remove test text

--- src/DOM-Elements/main.js
+++ src/DOM-Elements/main.js
@@ -2,12 +2,5 @@ export default (() => {
   const container = document.createElement('main');
   container.textContent = 'MAIN';
 
-  // TEST
-  const ital = document.createElement('span');
-  ital.textContent = 'This will be italicized';
-  const norm = document.createElement('div');
-  norm.textContent = 'this won\'t';
-  container.append(ital, norm);
-
   return container;
 })();
