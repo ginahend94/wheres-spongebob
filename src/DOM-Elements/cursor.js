217e84bcfd046ed4fe4cca9242ee4f10cb003613
Gina Henderson <gina@ginahenderson.me>
2022-12-01 23:19:22 -0600

    Keep bubble inside picture

--- src/DOM-Elements/cursor.js
+++ src/DOM-Elements/cursor.js
@@ -10,23 +10,36 @@ const bubble = (() => {
 })();
 
 const bubbleFollow = (e) => {
-  // if x or y is outside main, return closest, stop updating
-  
+  let left;
+  let top;
   // take in x and y of cursor
   const cursorX = e.pageX;
   const cursorY = e.pageY;
   // divide bubble in half
   const bubbleCenter = 50;
-  // left is x - width
-  const left = cursorX - bubbleCenter;
+  // if x or y is outside main, return closest, stop updating
+  const main = document.querySelector('main');
+  if (cursorX < bubbleCenter) {
+    left = 0;
+  } else {
+    // left is x - width
+    left = cursorX - bubbleCenter;
+  }
   // top is y - height - header height
-  const header = parseInt(getComputedStyle(document.querySelector('header')).height, 10);
-  const top = cursorY - bubbleCenter - header;
+  const header = parseInt(
+    getComputedStyle(document.querySelector('header')).height,
+    10
+  );
+  if (cursorY < main.offsetTop + bubbleCenter) {
+    top = 0;
+  } else if (cursorY > header + main.offsetHeight - bubbleCenter) {
+    top = main.offsetHeight - 2 * bubbleCenter;
+  } else {
+    top = cursorY - bubbleCenter - header;
+  }
+
   bubble.style.top = `${top}px`;
   bubble.style.left = `${left}px`;
 };
 
-export {
-  bubble,
-  bubbleFollow,
-};
+export { bubble, bubbleFollow };
