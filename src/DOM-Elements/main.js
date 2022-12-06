77e20b12632b610b33beb1bfdeb0939ba8560ee6
Gina Henderson <gina@ginahenderson.me>
2022-12-06 08:01:31 -0600

    create fillcurtain fn

--- src/DOM-Elements/main.js
+++ src/DOM-Elements/main.js
@@ -2,26 +2,29 @@ import background from '../assets/images/background.jpg';
 import { stopGame, startGame, resetGame } from '../functions/game';
 import { bubble, bubbleFollow } from './cursor';
 
-export default (() => {
+const main = (() => {
   const container = document.createElement('main');
   const curtain = (() => {
     const cont = document.createElement('div');
     cont.classList.add('curtain');
-    const start = document.createElement('button');
-    const stop = document.createElement('button');
-    const reset = document.createElement('button');
-    start.textContent = 'Start';
-    stop.textContent = 'Stop';
-    reset.textContent = 'Reset';
-    start.addEventListener('click', startGame);
-    stop.addEventListener('click', stopGame);
-    reset.addEventListener('click', resetGame);
-    cont.append(
-      start,
-      stop,
-      reset,
-    );
-    return cont;
+    cont.textContent = 'Loading...';
+    const fillCurtain = () => {
+      const start = document.createElement('button');
+      const stop = document.createElement('button');
+      const reset = document.createElement('button');
+      start.textContent = 'Start';
+      stop.textContent = 'Stop';
+      reset.textContent = 'Reset';
+      start.addEventListener('click', startGame);
+      stop.addEventListener('click', stopGame);
+      reset.addEventListener('click', resetGame);
+      cont.innerHTML = '';
+      cont.append(start, stop, reset);
+    };
+    return {
+      cont,
+      fillCurtain,
+    };
   })();
   const bg = new Image();
   bg.src = background;
@@ -29,11 +32,18 @@ export default (() => {
   container.addEventListener('mousemove', bubbleFollow);
 
   container.append(
-    curtain,
+    curtain.cont,
     bg,
-    bubble,
-    // popup,
+    bubble
   );
 
-  return container;
+  return {
+    container,
+    fillCurtain: curtain.fillCurtain,
+  };
 })();
+
+const fillCurtain = () => main.fillCurtain();
+
+export default main.container;
+export { fillCurtain };
