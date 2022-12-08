c35b0a775d977e9c4dff982343b9a58fc099a15b
Gina Henderson <gina@ginahenderson.me>
2022-12-08 01:49:20 -0600

    Add toast error

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -1,6 +1,5 @@
 import loading from '../DOM-Elements/loading';
-
-// let isLoaded = false;
+import showToast from '../DOM-Elements/toast';
 
 // const getData = async () => {
 //   const url =
@@ -9,37 +8,33 @@ import loading from '../DOM-Elements/loading';
 //   try {
 //     const res = await fetch(url);
 //     json = await res.json();
-// //     isLoaded = !!json;
 //   } catch (err) {
 //     json = { characters: 'Error loading.', scores: 'Error loading' };
 //   }
 //   return json;
 // };
+
+// TEST
 async function getData() {
-  // TEST
   // eslint-disable-next-line no-promise-executor-return
   return new Promise((resolve) => setTimeout(async () => {
-    const url =
-      'https://raw.githubusercontent.com/ginahend94/wheres-spongebob/main/src/test.json';
-    const res = await fetch(url);
-    const json = await res.json();
-    // isLoaded = !!json;
-    resolve(json);
+    try {
+      const url =
+      'https://raw.githubusercontent.com/ginahend94/wheres-spongebob/main/src/test0.json';
+      const res = await fetch(url);
+      if (!res.ok) {
+        showToast('red', `Server Error ${res.status}: ${res.statusText}. \n\rPlease refresh or try again later.`);
+      } else {
+        const json = await res.json();
+        resolve(json);
+      }
+    } catch (e) {
+      showToast('red', 'Error');
+      resolve(e);
+    }
   }, 3000));
 }
 document.body.append(loading);
 const data = await getData();
 
-// const loaded = () =>
-//   new Promise((resolve) => {
-//     const check = setInterval(() => {
-//       console.log(isLoaded);
-//       if (isLoaded) {
-//         resolve();
-//         clearInterval(check);
-//       }
-//     }, 10);
-//   });
-
 export default data;
-// export { loaded };
