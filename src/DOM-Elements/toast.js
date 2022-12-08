da514adbc42eda2175adda487115636892dff389
Gina Henderson <gina@ginahenderson.me>
2022-12-08 01:46:52 -0600

    Add exit to toast

--- src/DOM-Elements/toast.js
+++ src/DOM-Elements/toast.js
@@ -2,6 +2,9 @@ const newToast = (bgColor = 'red', text = 'Error') => {
   const container = document.createElement('div');
   container.classList.add('toast', bgColor);
   container.textContent = text;
+  const exit = document.createElement('span');
+  exit.textContent = '\u2715';
+  container.append(exit);
   return container;
 };
 
@@ -9,13 +12,18 @@ const showToast = (bgColor = 'red', text = 'Error') => {
   const toast = newToast(bgColor, text);
   document.body.append(toast);
   setTimeout(() => toast.classList.add('show'), 5);
-  setTimeout(() => {
-    toast.classList.remove('show');
+  const hide = setTimeout(() => {
+    // toast.classList.remove('show'); // TEST
   }, 4000);
-  setTimeout(() => {
-    document.body.removeChild(toast);
+  const remove = setTimeout(() => {
+    // document.body.removeChild(toast); // TEST
   }, 4500);
+  toast.querySelector('span').addEventListener('click', () => {
+    clearTimeout(hide);
+    clearTimeout(remove);
+    toast.classList.remove('show');
+    setTimeout(() => document.body.removeChild(toast), 500);
+  });
 };
 
 export default showToast;
-export { newToast }; // TEST
